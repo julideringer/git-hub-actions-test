03e92c196cc79f7fa3e7459d304837adc3931887
JULIAN MONTORO ANGOSTO <julianmontoroangosto@MacBook-Pro-de-JULIAN-2.local>
2024-10-21 19:33:42 +0200

    add changes

--- lambdas/test1/git_hub_actions_test/hello_world.py
+++ lambdas/test1/git_hub_actions_test/hello_world.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': 'Hello, Worlds Julian test4jojojojojojohola\
-            Ttttt!!julijulitodanielit'
+            Ttttt!!julijuliantonella'
     }
