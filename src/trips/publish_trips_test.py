60d757688736e0da72eed6b6bb0883f9b7d262a6
JULIAN MONTORO ANGOSTO <julianmontoroangosto@MacBook-Pro-de-JULIAN-2.local>
2024-10-23 00:14:24 +0200

    add changes

--- src/trips/publish_trips_test.py
+++ src/trips/publish_trips_test.py
@@ -32,7 +32,7 @@ def parser_to_camel_case(raw_item):
 
 def lambda_handler(event, context= None):
     """lambda handler"""
-    print("hola")
+    print("hola holita")
     body = parser_to_camel_case(event['body-json'])
     trip_id = str(uuid.uuid4())
     body['trip_id'] = trip_id
