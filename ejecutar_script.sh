bd4ea3dc1d4db45d98b5e0ad866a8500f7875d3c
JULIAN MONTORO ANGOSTO <julianmontoroangosto@MacBook-Pro-de-JULIAN-2.local>
2024-10-31 09:26:24 +0100

    ad

--- ejecutar_script.sh
+++ ejecutar_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
- # Encuentra las dependencias cambiadas
- changed_dependencies=$(git diff --name-only HEAD^ HEAD | grep -E '^src/(common_tools|trips_tools)/' | grep '\.py$')
-echo "MOSTRAMOS changed_dependencies: $changed_dependencies"
+changed_dependencies=$(git diff --name-only HEAD^ HEAD | grep -E '^src/(common_tools|trips_tools)/' | grep '\.py$')
+echo "hola"
+echo $changed_dependencies
+# Verifica la longitud de la variable
+if [ ${#changed_dependencies} -gt 0 ]; then
+    echo "Se encontraron cambios en las dependencias: $changed_dependencies"
+else
+    echo "No se encontraron cambios en las dependencias."
+fi
